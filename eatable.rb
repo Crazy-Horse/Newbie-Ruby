0bde44b90fa2fbc265ffbd9578d6b643c49bdf68
Crazy-Horse <stuart.holland@sygmatechnology.com>
2011-07-13 15:10:16 -0400

    adding code for first assignment

---
+++ eatable.rb
@@ -0,0 +1,21 @@
+module Eatable
+  class Food
+    attr_accessor :type, :price
+    def initialize(type, price)
+      @type, @price = type, price
+    end
+    def self.spoiled?(date)
+      return true if date > Time.now
+      false
+    end
+  end
+  class ThaiFood < Food
+    def spicy?
+      "Heck yea!!"
+    end
+    def good?(amount)
+      return ture if amount > @price
+      false
+    end
+  end
+end
